8852531d6a2dc6cc4be2627f9934a2f95141968a
rajapulau <rajapulau@gmail.com>
2020-11-13 10:32:28 +0700

    remove pods folder

--- ios/Pods/Headers/Public/react-native-qiscus-meet/RNJitsiMeetView.h
+++
@@ -1,1 +0,0 @@
-../../../../../node_modules/react-native-qiscus-meet/ios/RNJitsiMeetView.h
--- ios/Pods/Target Support Files/react-native-qiscus-meet/react-native-qiscus-meet-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_react_native_qiscus_meet : NSObject
-@end
-@implementation PodsDummy_react_native_qiscus_meet
-@end
